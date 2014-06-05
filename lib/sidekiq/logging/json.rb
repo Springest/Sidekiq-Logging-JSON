6446b8ad98a4dfb537a66661af0920a5db3a13fd
Wouter de Vos <wouter@springest.com>
2014-06-05 14:45:28 +0200

    Init gem.

---
+++ lib/sidekiq/logging/json.rb
@@ -0,0 +1,9 @@
+require "sidekiq/logging/json/version"
+
+module Sidekiq
+  module Logging
+    module Json
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/sidekiq/logging/json/version.rb
@@ -0,0 +1,7 @@
+module Sidekiq
+  module Logging
+    module Json
+      VERSION = "0.0.1"
+    end
+  end
+end
