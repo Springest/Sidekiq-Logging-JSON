2d15a0beb5b00af7d69c63430ec229e8795a775c
Wouter de Vos <wouter@springest.com>
2014-06-12 12:29:50 +0200

    Changed config format to comply with @ notation in LogStash.

--- lib/sidekiq/logging/json.rb
+++ lib/sidekiq/logging/json.rb
@@ -10,26 +10,28 @@ module Sidekiq
         def call(severity, time, program_name, message)
           {
             '@timestamp' => time.utc.iso8601,
-            :pid => ::Process.pid,
-            :tid => "TID-#{Thread.current.object_id.to_s(36)}",
-            :context => "#{context}",
-            :severity => severity,
-            :program_name => program_name,
-            :type => 'sidekiq',
-            :status => nil,
-            :run_time => nil
+            '@fields' => {
+              :pid => ::Process.pid,
+              :tid => "TID-#{Thread.current.object_id.to_s(36)}",
+              :context => "#{context}",
+              :program_name => program_name
+            },
+            '@type' => 'sidekiq',
+            '@status' => nil,
+            '@severity' => severity,
+            '@run_time' => nil
           }.merge(process_message(message)).to_json + "\r\n"
         end
 
         def process_message(message)
           result = message.match(/INFO: (done|start|fail)(: ([0-9\.]+) sec)?$/)
 
-          return { message: message } unless result
+          return { '@message' => message } unless result
 
           {
-            :message => message,                       # The full message
-            :status => result[1],                      # start or done
-            :run_time => result[3] && result[3].to_f   # run time in seconds
+            '@message' => message,                       # The full message
+            '@status' => result[1],                      # start or done
+            '@run_time' => result[3] && result[3].to_f   # run time in seconds
           }
         end
       end
