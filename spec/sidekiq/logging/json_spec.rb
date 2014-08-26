3d19f88f0d4dd90109eaf2003a453136ccec7539
Wouter de Vos <wouter@springest.com>
2014-08-26 13:39:46 +0200

    Fix(#1): Logging Exceptions
    - Fixes for issue #1 where exceptions were not logged properly.
    - Fixes the specs

--- spec/sidekiq/logging/json_spec.rb
+++ spec/sidekiq/logging/json_spec.rb
@@ -2,30 +2,42 @@ require 'spec_helper'
 
 describe "Sidekiq::Logging::Json" do
   describe "process_message" do
-    subject { Sidekiq::Logging::Json::Logger.new.process_message(logentry) }
+    subject { JSON.parse(Sidekiq::Logging::Json::Logger.new.call(severity, time, program_name, logentry)) }
     let(:logentry) { "Some random message" }
-    let(:message) { subject[:message] }
-    let(:status) { subject[:status] }
-    let(:run_time) { subject[:run_time] }
+    let(:message) { subject["@message"] }
+    let(:status) { subject["@status"] }
+    let(:run_time) { subject["@run_time"] }
+    let(:severity) { "INFO" }
+    let(:time) { Time.now }
+    let(:program_name) { "RSpec" }
 
-    it { expect( message ).to eq(logentry) }
+    it { expect( message ).to match(/#{logentry}/) }
     it { expect( status ).to eq(nil) }
     it { expect( run_time ).to eq(nil) }
 
     context "start" do
-      let(:logentry) { "2014-06-05T12:38:55Z 8630 TID-osanfu5nw SubjectClickthroughTracker JID-e17a9e9ca0521bb74b423166 INFO: start" }
+      let(:logentry) { "start" }
 
-      it { expect( message ).to eq(logentry) }
+      it { expect( message ).to match(/#{logentry}/) }
       it { expect( status ).to eq("start") }
       it { expect( run_time ).to eq(nil) }
     end
 
     context "done" do
-      let(:logentry) { "2014-06-05T12:38:42Z 8630 TID-osammcf2k TrainingIndexer::Work JID-177066e96052c2314dcad8c7 INFO: done: 51.579 sec" }
+      let(:logentry) { "done: 51.579 sec" }
 
-      it { expect( message ).to eq(logentry) }
+      it { expect( message ).to match(/#{logentry}/) }
       it { expect( status ).to eq("done") }
       it { expect( run_time ).to eq(51.579) }
+    end
+
+    context "exception" do
+      let(:exception_message) { "This is the message that should be logged." }
+      let(:logentry) { NoMethodError.new( exception_message ) }
+
+      it { expect(message).to match( exception_message ) }
+      it { expect( status ).to eq("exception") }
+      it { expect( run_time ).to eq(nil) }
     end
   end
 end
